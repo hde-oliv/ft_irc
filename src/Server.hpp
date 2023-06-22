f97a639b95f6e01fdca94c9eeff1b0b2d4c65105
hde-oliv <henriquerocha@tutanota.com>
2023-06-22 00:05:24 -0300

    refactor: client variable in Server

--- src/Server.hpp
+++ src/Server.hpp
@@ -13,31 +13,30 @@
 #include "Client.hpp"
 
 #define MAX_CLIENTS 5
-#define BUFFER_SIZE 10000
 
 #include <vector>
 
 class Server {
-    public:
-    Server(void);
-    Server(std::string const &password, int const &port);
-    Server(Server const &ref);
-    ~Server(void);
-    Server &operator=(Server const &ref);
-    void    startServer();
+	public:
+	Server(void);
+	Server(std::string const &password, int const &port);
+	Server(Server const &ref);
+	~Server(void);
+	Server &operator=(Server const &ref);
+	void	startServer();
 
-    private:
-    void setupSocket();
-    void clientEventHandling();
-    void serverEventHandling();
+	private:
+	void setupSocket();
+	void clientEventHandling();
+	void serverEventHandling();
 
-    std::string          password;
-    int                  port;
-    int                  server_fd;
-    std::vector<Client>  clients;
-    std::vector<Channel> channels;
-    struct pollfd        fds[MAX_CLIENTS];
-    struct sockaddr_in   address;
+	std::string			 password;
+	int					 port;
+	int					 server_fd;
+	int					 clients;
+	std::vector<Channel> channels;
+	struct pollfd		 fds[MAX_CLIENTS];
+	struct sockaddr_in	 address;
 };
 
 #endif
