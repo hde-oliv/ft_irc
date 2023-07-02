fb91ffdcb415a724c371e35b1f528cdeaa384a8a
hde-oliv <henriquerocha@tutanota.com>
2023-07-02 15:34:43 -0300

    refactor: splitString

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -2,11 +2,30 @@
 
 std::vector<std::string> splitString(std::string& source) {
 	std::vector<std::string> tokens;
-	std::string				 single_token;
-	std::istringstream		 sourceStream(source);
-	while (std::getline(sourceStream, single_token, ' ')) {
-		tokens.push_back(single_token);
+	std::string				 singleToken;
+
+	// TODO: Prefix are neccesary?
+	if (source[0] == ':') {	 // Prefix
+		std::istringstream sourceStream(source);
+		std::getline(sourceStream, singleToken, ' ');
+		tokens.push_back(singleToken);
+		source = source.substr(1);
 	}
+
+	// TODO: Find a better solution later
+	size_t colon = source.find(":");
+	if (colon != std::string::npos) {
+		std::string beforeColon = source.substr(0, colon);
+		std::string afterColon	= source.substr(colon);
+
+		std::istringstream beforeColonSteam(beforeColon);
+
+		while (std::getline(beforeColonSteam, singleToken, ' ')) {
+			tokens.push_back(singleToken);
+		}
+		tokens.push_back(afterColon);
+	}
+
 	return tokens;
 }
 void panic(std::string caller, std::string msg, int mode) {
